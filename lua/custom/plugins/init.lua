6ae86b1cdd54d975e20cedc49a3cbba66515772a
danjer <dhvandenakker@live.nl>
2025-03-17 21:29:42 +0100

    adds custom keybinding for running django manage runserver

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+---- Diagnostic keymaps
+
+vim.keymap.set('n', '<leader>dr', ':split | terminal python manage.py runserver<CR>', { desc = '[d]jango manage.py [r]unserver' })
+
 return {
   require 'custom.plugins.none-ls',
 }
