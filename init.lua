131bb377a421a615ebf747ea959257152fbb78b8
danjer <dhvandenakker@live.nl>
2025-03-18 20:56:46 +0100

    scale windows using custom bindings

--- init.lua
+++ init.lua
@@ -188,6 +188,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-z>', ':vertical resize +5<CR>', { desc = '+ current window' })
+vim.keymap.set('n', '<C-x>', ':vertical resize -5<CR>', { desc = '- current window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
